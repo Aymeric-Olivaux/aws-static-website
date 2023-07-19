1b4035d514691fb77248f2a565dff6a8fee15bae
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-07-19 22:49:14 +0200

    d

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------- Data --- #
 
 data "aws_route53_zone" "aws_zone" {
-  name = "${var.subdomain_name}${var.domain_name}."
+  name = "${var.domain_name}."
 }
