93c9ef863b97ba6bcf76500ae014069a41a392e7
Alexis Boissiere <alexis.boissiere@epita.fr>
2023-07-19 22:36:15 +0200

    d

--- acm.tf
+++ acm.tf
@@ -1,9 +1,19 @@
+locals {
+  main_region          = "us-east-1"
+
+}
+
+provider "aws" {
+  region = local.main_region
+}
+
+
 module "website_acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
   providers = {
-    aws = aws.us-east-1
+    aws = aws.main_region
   }
 
   domain_name = "${var.subdomain_name}${var.domain_name}"
